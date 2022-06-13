3b213404e92d129b5ad3ff957b46bc726349fecb
André Luís Bordignon <albordignon@gmail.com>
2022-06-13 12:29:41 -0300

    Versão 1.0.0

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactTouchEvents from "react-touch-events";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import './Page.css';
 
-const Page = ({ image, nextPage, previousPage, currentPage }) => {
-    return (
-        <div className='Page'>
-            {currentPage != 1
-                ? <FontAwesomeIcon className='ArrowLeft' icon={faAngleLeft} onClick={previousPage} />
-                : null
-            }
-            <img src={image} />
-            {currentPage <= 4
-                ? <FontAwesomeIcon className='ArrowRight' icon={faAngleRight} onClick={nextPage} />
-                : null
-            }
+const Page = ({ image, nextPage, previousPage, currentPage, lastPage }) => {
+  const [styleImage, setStyleImage] = useState();
+
+  useEffect(() => {
+    // window.addEventListener("resize", this.updateDimensions.bind(this));
+    console.log(window);
+
+    const widthGreater = {
+      height: '100vh',
+      backgroundColor: 'black'
+    };
+
+    const heightGreater = {
+      width: '100%',
+      backgroundColor: 'red'
+    }
+
+    if (window.innerWidth > window.innerHeight) {
+      setStyleImage(widthGreater);
+      console.log('Comprimento é maior');
+    } else {
+      setStyleImage(heightGreater);
+      console.log('Altura é maior');
+    }
+
+
+  }, []);
+
+  console.log('Windows Width', window.innerWidth);
+  console.log('Windows Height', window.innerHeight);
+
+  return (
+    <div className='Page'>
+
+      {currentPage !== 1
+        ?
+        < div className='Arrow-Left-Container' onClick={previousPage}>
+
+          <FontAwesomeIcon className='ArrowLeft' icon={faAngleLeft} onClick={previousPage} size="2x" color='#BD3D21' />
         </div>
-    );
+        : null
+      }
+      <div className='Image-Container'>
+        <img style={styleImage} src={image} alt='' />
+      </div>
+      {currentPage < lastPage
+        ?
+        <div className='Arrow-Right-Container' onClick={nextPage}>
+
+          <FontAwesomeIcon className='ArrowRight' icon={faAngleRight} onClick={nextPage} size="2x" color='#BD3D21' />
+        </div>
+        : null
+      }
+    </div>
+  );
 };
 
 export default Page;
--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Page from '../Page/Page';
-import Page1 from '../../assets/images/page1.png';
-import Page2 from '../../assets/images/page2.png';
-import Page3 from '../../assets/images/page3.png';
-import Page4 from '../../assets/images/page4.png';
-// import Page5 from '../../assets/images/page5.png';
-// import Page6 from '../../assets/images/page6.png';
-// import Page7 from '../../assets/images/page7.png';
-// import Page8 from '../../assets/images/page8.png';
+import Page01 from '../../assets/images/page01.jpeg';
+import Page02 from '../../assets/images/page02.jpeg';
+import Page03 from '../../assets/images/page03.jpeg';
+import Page04 from '../../assets/images/page04.jpeg';
+import Page05 from '../../assets/images/page05.jpeg';
+import Page06 from '../../assets/images/page06.jpeg';
+import Page07 from '../../assets/images/page07.jpeg';
+import Page08 from '../../assets/images/page08.jpeg';
+import Page09 from '../../assets/images/page09.jpeg';
+import Page10 from '../../assets/images/page10.jpeg';
+import Page11 from '../../assets/images/page11.jpeg';
+import Page12 from '../../assets/images/page12.jpeg';
+import Page13 from '../../assets/images/page13.jpeg';
+import Page14 from '../../assets/images/page14.jpeg';
+import Page15 from '../../assets/images/page15.jpeg';
+import Page16 from '../../assets/images/page16.jpeg';
 
 const Pages = () => {
 
     const [pagesImages, setPagesImages] = useState(
-        [Page1, Page2, Page3, Page4]
+        [Page01, Page02, Page03, Page04, Page05, Page06, Page07, Page08, Page09, Page10, Page11, Page12, Page13, Page14, Page15, Page16]
     );
     const [currentPage, setCurrentPage] = useState(1);
-    const [lastPage, setLastPage] = useState(4);
-    const [pages, setPages] = useState(16);
+    const [pages, setPages] = useState();
     const [currentPageImage, setCurrentPageImage] = useState(pagesImages[0]);
 
 //         , Page5, Page6,Page7, Page8, Page9,Page10, Page11, Page12,Page13, Page14, Page15,Page16]
 // )
+
+useEffect(() => {
+  setPages(16);
+})
 
 const previousPage = () => {
     let newCurrentPage = currentPage - 1;
@@ -42,6 +53,7 @@ return (
             nextPage={nextPage}
             previousPage={previousPage}
             currentPage={currentPage}
+            lastPage={pages}
         />
     </div>
 );
